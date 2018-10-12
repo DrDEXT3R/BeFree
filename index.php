ba3f66799ce77909a0d30cf9cce5b0ec1d1fba67
Tomasz Strzoda <strzoda.tomasz@gmail.com>
2018-10-12 23:24:43 +0200

    Add pop up login

--- index.php
+++ index.php
@@ -8,39 +8,27 @@
 		<meta name="author" content="Tomasz Strzoda">		
 		<meta http-equiv="X-Ua-Compatible" content="IE-edge,chrome=1">
 		<link rel="stylesheet" href="css/main.css">	
+		<link rel="stylesheet" href="css/loginPopUp.css">	
 		<script src="code.js"></script>
 	</head>
 	<body>
 		<div class="container">
 			<div class="logo">
-			
 				beFree
-				<!-- <button type="button">Click Me!</button> -->
-				
-				<div class=logowanie>Log in</div>
-				
-				<a href="javascript:void(0)" class="button button1" onclick="showLoginPopup()">Logowanie</a>
-				
+				<button onclick="document.getElementById('modal-wrapper').style.display='block'" style="width:100px;">Log in</button>
+				<div id="modal-wrapper" class="modal">
+				  <form class="modal-content animate" action="logIn.php" method="post">  
+					<div class="popUpContainer">
+					  <span onclick="document.getElementById('modal-wrapper').style.display='none'" class="close" title="Close PopUp">&times;</span>
+						<img src="img/avatar.jpg" alt="Avatar" class="avatar">
+					  <input type="text" placeholder="Enter login" name="login">
+					  <input type="password" placeholder="Enter password" name="password">        
+					  <button class="popUpLogIn" type="submit">Log in</button>
+					</div>
+				  </form>
+				</div>
 			</div>
-			
-			
-			
-			
-			<?php
-				$forename = "Joanna";
-				echo "$forename, Hello World";
-			?>
-			
-			<form action="logIn.php" method="post">
-				Login <br/><input type="text" name="login"/><br/><br/>
-				Password <br/><input type="password" name="password"/><br/><br/>
-				<input type="submit" value="Log in"/>
-			</form>
-	
-	
-			
-			
-			
+			<div class="menu"></div>			
 			<a href="employer.html" class="button button1">
 				<div class="kurs">
 					<div class="test"></div>
@@ -49,7 +37,6 @@
 					Szukasz zleceń
 				</div>
 			</a>
-		
 			<a href="freelancer.html" class="button button1">
 				<div class="kurs">
 					<h1>przycisk2</h1>
@@ -57,9 +44,6 @@
 					Szukasz zleceń
 				</div>	
 			</a>
-			
-			
-			<div class="menu"></div>
 		</div>
 	</body>
 </html>
